6491779ecb47f940c9fad523c8a1ad63de8d2500
Justin Silverman <jsilve24@gmail.com>
2018-11-28 12:35:11 -0500

    Faster fillUnitNormal slightly speed up uncollapse

--- src/test_utils.cpp
+++ src/test_utils.cpp
@@ -2,6 +2,8 @@
 #include <MatDist.h>
 using namespace Rcpp;
 
+// [[Rcpp::depends(RcppZiggurat)]]
+
 // [[Rcpp::export]]
 void fillUnitNormal_test(Eigen::Map<Eigen::MatrixXd>& Z){
   fillUnitNormal(Z);
